75e782630f50b695be15f31dfa8c477ada53d303
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-15 13:36:49 -0300

    Login and Register

--- DotnetAPI/Data/DataContextDapper.cs
+++ DotnetAPI/Data/DataContextDapper.cs
@@ -4,10 +4,9 @@ using Microsoft.Data.SqlClient;
 
 namespace DotnetAPI.Data
 {
-    public class DataContextDapper
+    class DataContextDapper
     {
         private readonly IConfiguration _config;
-
         public DataContextDapper(IConfiguration config)
         {
             _config = config;
@@ -35,6 +34,27 @@ namespace DotnetAPI.Data
         {
             IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             return dbConnection.Execute(sql);
+        }
+
+        public bool ExecuteSqlWithParameters(string sql, List<SqlParameter> parameters)
+        {
+            SqlCommand commandWithParams = new SqlCommand(sql);
+
+            foreach (SqlParameter parameter in parameters)
+            {
+                commandWithParams.Parameters.Add(parameter);
+            }
+
+            SqlConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            dbConnection.Open();
+
+            commandWithParams.Connection = dbConnection;
+
+            int rowsAffected = commandWithParams.ExecuteNonQuery();
+
+            dbConnection.Close();
+
+            return rowsAffected > 0;
         }
     }
 }
